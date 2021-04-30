fa9dd83eb9e5da8c9fb28034e1266229b751aa10
uripast <44765926+uripast@users.noreply.github.com>
2021-04-30 17:46:07 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+#use config file to run the params from outside
+params = {'access_key': '2044e3ef665718f351d2a89204b2a224','query': 'Eilat'}
--- weather.py
+++ weather.py
@@ -1,58 +1,28 @@
 import requests
 import sqlite3
 import json
+import config
 
-params = {
-  'access_key': '2044e3ef665718f351d2a89204b2a224',
-  'query': 'Eilat'
-}
-api_result = requests.get('http://api.weatherstack.com/current', params)
-#print(api_result)
-# getting data in the json format
+api_result = requests.get('http://api.weatherstack.com/current', config.params)
+#test successful response
 if api_result.status_code == 200:
    api_response = api_result.json()
 
+#data is a json file
    data = json.dumps(api_response)
-#print(data)
+   response_dict = json.loads(data)
 
-"""   # getting the main dict block
-   location = api_response['location']
-   current = api_response['current']
-
-# enter city
-   Region = location['region']
-   temp = current['temperature']
-
-   print(f"{Region:-^30}")
-   print(f"temperature: {temp}")
-
-else:
-   #error in http request
-   print("Error in the HTTP request")
-"""
+#connect to sqlite
 conn = sqlite3.connect('test.db')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS stackweather (city varchar(3), data json)")
-c.execute("insert into stackweather values (?, ?)",["1", json.dumps(api_response)])
+c.execute("CREATE TABLE IF NOT EXISTS stack_weather (city varchar(30), country varchar(30),region varchar(30), temp integer)")
+c.execute("insert into stack_weather values (?,?,?,?)",
+          [response_dict["location"]['name'],
+           response_dict["location"]['country'],
+           response_dict["location"]['region'],
+           response_dict["current"]['temperature']])
 conn.commit()
-c.execute("select * from stackweather")
+c.execute("select * from stack_weather")
 print(c.fetchone())
-#conn.close()
-
-"""
-for city in data:
-    c.execute("insert into stackweather values (?, ?)",
-    [city[0], json.dumps(api_response)])
-    conn.commit()
-
-
-c.execute("select * from stackweather;")
 conn.close()
 
-def get_my_jsonified_data(key):
-    with sqlite3.connect('test.db') as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM stackweather WHERE column=?;", [city])
-        datad = cursor.fetchall()
-        print (json.dumps(datad))
-"""
